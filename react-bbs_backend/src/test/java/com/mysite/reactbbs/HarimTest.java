09256bf805c139feb40d0bc4e3bee0114fab996a
HarimGo <gkfla4466@gmail.com>
2025-07-15 12:00:10 +0900

    harim coommit - ghr (#5)
    Co-authored-by: Harim Go <gkfla4655@naver.com>

---
+++ react-bbs_backend/src/test/java/com/mysite/reactbbs/HarimTest.java
@@ -0,0 +1,12 @@
+package com.mysite.reactbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class HarimTest {
+
+	@Test
+	void harimTest() {
+		System.out.println("Harim");
+	}
+}
